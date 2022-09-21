6d3e6cea231f21f02a68df1508f884e5d21270ed
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-09-21 21:14:58 +0300

    init commit added lesson1 files

---
+++ lesson_1/lec.py
@@ -0,0 +1,27 @@
+# a = 123
+# b = 1.23
+
+# s = "hello world"
+# print(s)
+
+# print(a, ' - ', b, ' - ', s)
+# print('{2} - {1} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+
+# f = True
+# print(f)
+
+# list = []
+# print(list)
+
+
+# print('enter number a')
+# a = int(input())
+# print('enter number b')
+# b = int(input())
+
+# print(a, '+', b, '=', a + b)
+
+
+
