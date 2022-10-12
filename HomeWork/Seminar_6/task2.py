feb985b26881c8fec8855eec4d12020b7868524d
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2022-10-12 22:27:14 +0300

    update

--- HomeWork/Seminar_6/task2.py
+++ HomeWork/Seminar_6/task2.py
@@ -1,34 +1,23 @@
 '''
 2. Для чисел в пределах от 20 до N найти числа, кратные 20 или 21. Use comprehension.
+in
+100
+
+out
+[20, 21, 40, 42, 60, 63, 80, 84, 100]
 '''
 import random
 
 
-# ВАРИАНТ 1
 # Функция, проверяет кратность данного числа, числам 20 и 21
-def check_div_nums(num):
-	return not num % 20 or not num % 21
+check_div_nums = lambda num: not num % 20 or not num % 21
 
-# Функция, заполняет список числами от 20 до указанного значения'''
-def fill_list(range_max_value):
-	return [i for i in range(20, range_max_value)]
+# Функция, принимает список чисел, проверяет их кратность 20 и 21, и возвращет новый список
+def list_num_from_20_to_N(list_in):
+	return [i for i in list_in if check_div_nums(i)]
 
-list_in = fill_list(100)
-print(f'source list: \n{list_in} ')
+range_max_value = int(input('enter max range of numbers: '))
+list_in = [i for i in range(20, range_max_value + 1)]
 
-list_out = [i for i in list_in if check_div_nums(i)]
-print(f'output list: \n{list_out}')
-
-
-print('=' * 50)
-
-# ВАРИАНТ 2 (с генерацией списка случайных чисел)
-range_max_value = int(input('enter max range: '))
-
-div_nums = lambda num: not num % 20 or not num % 21
-
-list_in = [random.randint(1, 100) for _ in range(range_max_value)]
-print(f'output list: \n{list_in}')
-
-list_out = [i for i in list_in if div_nums(i)]
+list_out = list_num_from_20_to_N(list_in)
 print(f'output list: \n{list_out}')
